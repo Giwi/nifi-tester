760c628e882647fe79c43f95ad37e0a0c8247f68
Giwi <marin.xavier@gmail.com>
2026-04-29 22:06:22 +0200

    Add Groovy script pipeline sample with test case

--- src/test/java/org/giwi/nifi/client/NiFiIntegrationTest.java
+++ src/test/java/org/giwi/nifi/client/NiFiIntegrationTest.java
@@ -62,7 +62,53 @@ class NiFiIntegrationTest {
             String pgId = result.getProcessGroupId();
             var group = tester.getProcessGroup(pgId);
             assertNotNull(group);
-            
+
+            // Cleanup
+            tester.deleteProcessGroup(pgId);
+        }
+    }
+
+    @Test
+    @DisplayName("Deploy Groovy script pipeline")
+    void testGroovyScriptPipeline() throws Exception {
+        var result = tester.deployPipeline(
+            new java.io.File("src/test/resources/pipelines/groovy-script-pipeline.yaml"),
+            "root"
+        );
+
+        assertNotNull(result);
+        assertTrue(result.isSuccess());
+
+        if (result.isSuccess()) {
+            String pgId = result.getProcessGroupId();
+            var group = tester.getProcessGroup(pgId);
+            assertNotNull(group);
+
+            // Start the process group to generate flow files
+            tester.startProcessGroup(pgId);
+
+            // Wait for some processing to happen
+            Thread.sleep(3000);
+
+            // Stop the process group
+            tester.stopProcessGroup(pgId);
+
+            // Find the connection between AppendWorld and LogResult
+            String connId = tester.findConnectionId(pgId, "AppendWorld", "LogResult");
+
+            if (connId != null) {
+                // Check the content of flow files in the queue
+                var contents = tester.getFlowFileContentsAsString(connId);
+                boolean found = false;
+                for (String content : contents) {
+                    if (content.contains("hello world")) {
+                        found = true;
+                        break;
+                    }
+                }
+                assertTrue(found, "Expected to find 'hello world' in flow file content");
+            }
+
             // Cleanup
             tester.deleteProcessGroup(pgId);
         }
